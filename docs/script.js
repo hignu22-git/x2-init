5a3b2e3be61ec4fd141a12c0e23a2f81970daf97
just-Synjin <bogdan26062007gold@gmail.com>
2023-11-27 13:05:40 +0200

    jobanuy jepta script, dark theme 50%(complete)

---
+++ docs/script.js
@@ -0,0 +1,21 @@
+const themeSwitchers = document.querySelectorAll('.changeTheme');
+
+themeSwitchers.forEach(switcher => {
+    switcher.addEventListener('click', function() { 
+        applyTheme(this.dataset.theme);
+        localStorage.setItem('theme', this.dataset.theme);
+    });
+});
+
+function applyTheme(themeName){
+    let themeUrl = `css/${themeName}.css`;
+    document.querySelector('[title="theme"]').setAttribute('href', themeUrl);
+}
+
+let activeTheme = localStorage.getItem('theme');
+
+if(activeTheme === null) {
+    applyTheme('light');
+}else {
+    applyTheme(activeTheme);
+}
